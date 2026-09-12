dc01052c5410ede15813a2990e155a6bde0a8594
himani27301 <himajaak8@gmail.com>
2026-09-12 12:23:48 +0530

    Make HearCare installable on mobile

--- web/app/layout.js
+++ web/app/layout.js
@@ -2,7 +2,20 @@ import './globals.css';
 
 export const metadata = {
   title: 'HearCare Pro | Raghavendra Speech and Hearing Center',
-  description: 'Clinic management for Raghavendra Speech and Hearing Center, Hyderabad'
+  description: 'Clinic management and patient portal for Raghavendra Speech and Hearing Center, Hyderabad',
+  manifest: '/manifest.webmanifest',
+  icons: {
+    icon: '/hearcare-icon.svg',
+    apple: '/hearcare-icon.svg'
+  },
+  applicationName: 'Raghavendra HearCare Pro'
+};
+
+export const viewport = {
+  themeColor: '#123f68',
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover'
 };
 
 export default function RootLayout({ children }) {
